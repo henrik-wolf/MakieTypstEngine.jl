134f2a66ec3234a0e78d7c68ac03dbd75f638c99
Henrik Wolf <h.wolf@amolf.nl>
2025-10-03 16:08:55 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.56

---
+++ src/MakieTypstEngine.jl
@@ -0,0 +1,5 @@
+module MakieTypstEngine
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MakieTypstEngine
+using Test
+
+@testset "MakieTypstEngine.jl" begin
+    # Write your tests here.
+end
