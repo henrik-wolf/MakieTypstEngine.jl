7c6c1f614b48777ae9ceeb47797b0edde20de8a0
Henrik Wolf <h.wolf@amolf.nl>
2025-10-14 17:41:26 +0200

    bit of cleanup in the cli glue

--- src/rust_cli.jl
+++ src/rust_cli.jl
@@ -1,7 +1,13 @@
 include("general_utils.jl")
 
 # get_run_cmd(paths = []) = length(paths) > 0 ? `cargo run -- $paths` : `cargo run`
-# get_run_cmd() = cli_binary[]
+# function get_run_cmd(paths = [])
+#     if length(paths) > 0
+#         `$(cli_binary[]) $paths`
+#     else
+#         cli_binary[]
+#     end
+# end
 function get_run_cmd(paths = [])
     if length(paths) > 0
         `target/release/layout-cli $paths`
@@ -9,12 +15,12 @@ function get_run_cmd(paths = [])
         `target/release/layout-cli`
     end
 end
+
 ## Copied from https://discourse.julialang.org/t/capture-stdout-and-stderr-in-case-a-command-fails/101772/3
-function execute(cmd::Cmd; input = nothing, path = ".")
+function execute(cmd::Cmd; inputcmd, path = ".")
     out = Pipe()
     err = Pipe()
 
-    inputcmd = isnothing(input) ? `echo ''` : input
     cd(path) do
         process =
             run(pipeline(ignorestatus(cmd); stdin = inputcmd, stdout = out, stderr = err))
@@ -36,32 +42,8 @@ Compile a string that looks like a full typst document into a json object of the
 function compile_string(str, additional_font_paths = [])
     runcmd = get_run_cmd(additional_font_paths)
     path = get_rust_dir()
-    input_cmd = `echo $(str)` # definitely not safe
+    input_cmd = `echo $(str)` # TODO: definitely not safe
 
     output = execute(runcmd; input = input_cmd, path = path)
     return JSON.parse(output)
 end
-
-"""
-Compile a typst file at `filename` into a json object of the layout
-"""
-function compile_file(filename)
-    runcmd = get_run_cmd()
-    path = get_rust_dir()
-    new_filename = relpath(filename, path)
-    input_cmd = `cat '$(escape_string(str))'` # definitely not safe
-
-    output = execute(runcmd; input = input_cmd, path = path)
-    return JSON.parse(output.stdout)
-end
-
-# compile_file(joinpath(get_rust_dir(), "template.typ"))
-
-# example_str = raw"""// template.typ
-# #set page(paper: "a4")
-# #set text(font: "Fira Math", 11pt)
-# #show math.equation: set text(font: "Fira Math")
-# $sum x/y$
-# """
-# escape_string(example_str)
-# output, errput = compile_string(example_str)
